1119d886435892d6515b2d3819c98438e1bf3ecd
vijaymarka <87945757+vijaymarka@users.noreply.github.com>
2022-07-15 11:12:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req, res) => {
-res.send("Welcome to Home Page");
+res.send("Welcome to Home Page.. This is Hosting from Aws Pipeline");
 })
 
 const port = process.env.port || 3000;
