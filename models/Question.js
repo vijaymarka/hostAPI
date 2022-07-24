df88a2a53fc896d29d4dd1da7ce8696a55f138da
vijaymarka <87945757+vijaymarka@users.noreply.github.com>
2022-07-24 13:58:59 +0530

    Update Question.js

--- models/Question.js
+++ models/Question.js
@@ -3,7 +3,14 @@ const mongoose = require("mongoose");
 const questionSchema = new mongoose.Schema({
   question: { type: String },
   options: Array,
+  type: {
+    type: String,
+  },
   correct_option: { type: String },
+  right_answer: Array,
+  type: {
+    type: String,
+  },
   category: { type: String },
   tag: Array,
   type: {
